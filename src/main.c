149104ccdcce1085a2b37a1a05835ac12b5f28d9
alex <alex@qr.com>
2019-10-21 11:48:23 +0800

    capture app

---
+++ src/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2019年10月18日
+ *      Author: jet
+ */
+
+
+
+#include "main.h"
+
+
+int main_05(int argc, char **argv)
+{
+	//M_MAIN(argc, argv);
+}
---
+++ src/main.h
@@ -0,0 +1,21 @@
+/*
+ * main.h
+ *
+ *  Created on: 2019年10月18日
+ *      Author: jet
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+
+//#define M_MAIN main_01
+
+
+//int main_01(int argc, char **argv);
+
+
+
+
+
+#endif /* MAIN_H_ */
